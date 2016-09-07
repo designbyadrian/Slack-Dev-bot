0e5b9666d5e46478cc7bb22fc0b31069f8115e3a
Adrian von Gegerfelt <adrian@designbyadrian.com>
2016-09-07 13:18:32 +0100

    Boom!

--- flows/chatter.js
+++ flows/chatter.js
@@ -8,33 +8,69 @@ module.exports = (slapp) => {
 
   let searching = false
 
-/*  slapp.message('*', (msg, text) => {
-    msg.say("You said: "+text);
-  });*/
-
   slapp.command('/urban', (msg) => {
     console.log("COMMAND");
-    msg
-      .say(`Echo, "${msg.body.text}"`)
+
+    if(!searching) {
+      msg.say("I'm busy!");
+      return false;
+    } else {
+      searching = true;
+    }
+
+    request({
+        url: defineUrl+msg.body.text,
+        json: true
+      }, (error,response,body)=>{
+
+        searching = false;
+
+        if(!error && response.statusCode === 200) {
+
+          try {
+
+            if(body.list.length<1) {
+              msg.say(`I don't know what the fuck "${msg.body.text}" is!`);
+            } else {
+              msg.say(body.list[0].definition);
+            }
+
+            
+          } catch(e) {
+            console.log(e.message);
+          }
+        }
+      })
+
   })
 
   slapp.message('.*', (msg) => {
     console.log("MESSAGE");
 
-    request({
-      url: randomUrl,
-      json: true
-    }, (error,response,body)=>{
-      if(!error && response.statusCode === 200) {
-        console.log(body)
-        try {
-          console.log(body.list[0].example)
-        } catch(e) {
-          console.log(e.message);
-        }
-      }
-    })
+    if(Math.random()<0.1&&!searching) {
 
+      searching = true;
+
+      request({
+        url: randomUrl,
+        json: true
+      }, (error,response,body)=>{
+
+        searching = false;
+
+        if(!error && response.statusCode === 200) {
+
+          try {
+            var index = Math.floor(Math.random()*body.list.length);
+
+            msg.say(body.list[index].example)
+          } catch(e) {
+            console.log(e.message);
+          }
+        }
+      })
+
+    }
     
   })
 
